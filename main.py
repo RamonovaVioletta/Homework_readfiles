a8120fd5bc5a469f7acbaadb8c250690ae6bc548
RamonovaVioletta <163902400+RamonovaVioletta@users.noreply.github.com>
2024-04-09 15:55:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+def get_shop_list_by_dishes(dishes, person_count):
+    shop_list = {}
+    for my_dish in dishes:
+        if my_dish in cook_book:
+            my_ingredients = cook_book[my_dish]
+            for ingredient in my_ingredients:
+                name = ingredient['ingredient_name']
+                measure = ingredient['measure']
+                quantity = int(ingredient['quantity']) * person_count
+                if name in shop_list:
+                    shop_list[name]['quantity'] += quantity
+                else:
+                    shop_list[name] = {'measure': measure, 'quantity': quantity}
+    return shop_list
+
+
 cook_book = {}
 with open('test.txt') as f:
     # Читаем файл test.txt и возвращаем список
@@ -8,15 +24,17 @@ with open('test.txt') as f:
         # Сохраняем в переменную values ингредиенты, количество, единицу измерения и возвращаем список
         values = food.split('\n')[2:]
         # Создаем список ингредиентов
-        cb_ingridients = []
+        cb_ingredients = []
 
         for value in values:
-            ingridient = {}
+            ingredient = {}
             value = value.split('|')
-            ingridient['ingridient_name'] = value[0]
-            ingridient['quantity'] = value[1]
-            ingridient['measure'] = value[2]
-            cb_ingridients.append(ingridient)
-        cook_book[key] = cb_ingridients
+            ingredient['ingredient_name'] = value[0]
+            ingredient['quantity'] = value[1]
+            ingredient['measure'] = value[2]
+            cb_ingredients.append(ingredient)
+        cook_book[key] = cb_ingredients
 
-print(cook_book)
+
+result = get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
+print(result)
